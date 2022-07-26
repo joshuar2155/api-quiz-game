9cd15e235cfad7b405c93f1e93ccce314687a94d
Josh Rae <joshuar2155@gmail.com>
2022-07-26 16:30:28 -0400

    question variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
-var = 
-var = 
-var = 
-var = 
-var = 
-var = 
-var = 
-var = 
-var = 
-var =
-var =
+
+
+
+var firstquestion = {
+  question: "What's 1+1?",
+  choices: ["2", "3", "4", "1"],
+  correct: "2",
+};
+var secondquestion = {
+  question: "Which country is in europe?",
+  choices: ["Colombia", "Laos", "Eqypt", "Estonia"],
+  correct: "Estonia",
+};
+var thirdquestion = {
+  question: "Which is a fruit?",
+  choices: ["Apple", "Carrot", "Steak", "Bread"],
+  correct: "Apple",
+};
+var fourthquestion = {
+  question: "Which letter comes first in the Alphabet?",
+  choices: ["Z", "J", "G", "E"],
+  correct: "E",
+};
+
